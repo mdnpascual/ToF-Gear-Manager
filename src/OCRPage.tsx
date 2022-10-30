f825f7ac8af7774292de8fc323ad6f54f7678127
mdnpascual <mdnpascual@yahoo.com.ph>
2022-10-30 03:34:07 -0600

    connect tesseractJS

--- src/OCRPage.tsx
+++ src/OCRPage.tsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import { createScheduler, createWorker } from 'tesseract.js';
-
-const worker = createWorker({
-	logger: m => console.log(m)
-});
-
-const ocrWorker = (async () => {
-	await worker.load();
-	await worker.loadLanguage('eng');
-	await worker.initialize('eng');
-	const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-	console.log(text);
-	await worker.terminate();
-})()
+import { parseOCR }from './OCRParser';
 
 export function OCRPage() {
 	const divRef = useRef<HTMLDivElement>(null);
 
-	const [count, setImageData] = React.useState(0);
+	const [imageFile, setImageData] = React.useState(new File([""], "filename"));
+
+	const worker = createWorker({
+		// logger: m => console.log(m)
+	});
+
+	const ocrWorker = (async () => {
+		await worker.load();
+		await worker.loadLanguage('eng');
+		await worker.initialize('eng');
+		await worker.setParameters({
+			tessedit_char_whitelist: '0123456789+:ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+		});
+		const { data: { text } } = await worker.recognize(imageFile);
+		parseOCR(text);
+		await worker.terminate();
+	})()
 
 	useEffect(() => {
 		const handleDrag = (e: Event) => {
@@ -36,19 +40,22 @@ export function OCRPage() {
 			e.stopPropagation()
 			console.log("handleDragOut")
 		}
-		const handleDrop =  (e: Event) => {
+		const handleDrop =  (e: DragEvent) => {
 			e.preventDefault()
 			e.stopPropagation()
 			console.log("handleDrop")
-			console.log(e)
+			var clipboardData = e.dataTransfer;
+			console.log(clipboardData!.files[0]);
+			setImageData(clipboardData!.files[0]);
 		}
 		const handlePaste = (e: ClipboardEvent) => {
 			e.preventDefault()
 			e.stopPropagation()
 			console.log("handlePaste")
 			console.log(e)
-			var items = e.clipboardData?.items;
-  			console.log(JSON.stringify(items)); // will give you the mime types
+			var clipboardData = e.clipboardData;
+			console.log(clipboardData!.files[0]);
+			setImageData(clipboardData!.files[0]);
 		}
 
 		const ocrElement = divRef.current;
---
+++ src/OCRParser.ts
@@ -0,0 +1,9 @@
+export const parseOCR = (unclean: string) => {
+	console.log("In parser: " + unclean)
+	// CS:
+	// Random stats
+	// Stat1
+	// Stat2
+	// Stat3
+	// Stat4
+}
